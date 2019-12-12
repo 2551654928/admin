30a4c97a660f08952d7d43a505b3064748975325
WispX <1591788658@qq.com>
2019-12-12 13:32:59 +0800

    :sparkles: 改进自动检测功能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -37,65 +37,66 @@ class TaskController extends Controller
         }
 
         $cache = 'inspect_time';
-        Cache::forget($cache);
-        /*if (Cache::has($cache)) {
+        if (Cache::has($cache)) {
             return ['code' => 0, 'msg' => '未到下一个周期'];
         }
-        Cache::put($cache, time(), (3600 * $options['auto_writing_period']));*/
+        Cache::put($cache, time(), (3600 * $options['auto_writing_period']));
 
-        $blogs = Blog::where('status', 1)->cursor();
         $client = new Client([
             'timeout' => 30,
             'verify' => false,
             'allow_redirects' => false,
         ]);
 
-        $results = $datelines = $promises = [];
-
+        set_time_limit(0);
+        ignore_user_abort(true);
         date_default_timezone_set('Asia/Shanghai');
         DB::beginTransaction();
-        foreach ($blogs as $blog) {
-            $promises[] = $client->getAsync($blog->link)->then(
-                function (ResponseInterface $res) {},
-                function (RequestException $e) use ($blog, $options, &$results, &$datelines) {
-                    // 如果上次异常时间不在今天, 则异常次数从 1 开始, 并更新异常时间
-                    if (date('Y-m-d') !== date('Y-m-d', $blog->abnormal_at)) {
-                        $data = ['abnormal_num' => 1, 'abnormal_at' => time()];
-                    } else {
-                        $currentAbnormalNum = $blog->abnormal_num + 1;
-                        // 是否已超出当天最大异常次数(加上当前异常)
-                        if ($currentAbnormalNum >= $options['max_abnormal_num']) {
-                            $data = ['status' => 3, 'abnormal_num' => $currentAbnormalNum, 'abnormal_at' => time()];
-                            /*// 是否自动写入异常大事记
-                            if ($options['auto_writing_dateline']) {
-                                $date = date('Y-m-d H:i:s');
-                                $datelines[] = [
-                                    'blog_id' => $blog->id,
-                                    'date' => $date,
-                                    'content' => "经系统爬虫自动检测，站点发生异常无法访问(或被重定向)，标记异常({$e->getMessage()})",
-                                    'updated_at' => $date,
-                                    'created_at' => $date
-                                ];
-                            }*/
+        Blog::where('status', 1)->chunk(10, function ($blogs) use (&$client, &$options) {
+            $results = $datelines = $promises = [];
+
+            foreach ($blogs as $blog) {
+                $promises[] = $client->getAsync($blog->link)->then(
+                    function (ResponseInterface $res) {
+                    },
+                    function (RequestException $e) use ($blog, $options, &$results, &$datelines) {
+                        // 如果上次异常时间不在今天, 则异常次数从 1 开始, 并更新异常时间
+                        if (date('Y-m-d') !== date('Y-m-d', $blog->abnormal_at)) {
+                            $data = ['abnormal_num' => 1, 'abnormal_at' => time()];
                         } else {
-                            // 增加异常数量 and 更新异常时间
-                            $data = ['abnormal_num' => $currentAbnormalNum, 'abnormal_at' => time()];
+                            $currentAbnormalNum = $blog->abnormal_num + 1;
+                            // 是否已超出当天最大异常次数(加上当前异常)
+                            if ($currentAbnormalNum >= $options['max_abnormal_num']) {
+                                $data = ['status' => 3, 'abnormal_num' => $currentAbnormalNum, 'abnormal_at' => time()];
+                                /*// 是否自动写入异常大事记
+                                if ($options['auto_writing_dateline']) {
+                                    $date = date('Y-m-d H:i:s');
+                                    $datelines[] = [
+                                        'blog_id' => $blog->id,
+                                        'date' => $date,
+                                        'content' => "经系统爬虫自动检测，站点发生异常无法访问(或被重定向)，标记异常({$e->getMessage()})",
+                                        'updated_at' => $date,
+                                        'created_at' => $date
+                                    ];
+                                }*/
+                            } else {
+                                // 增加异常数量 and 更新异常时间
+                                $data = ['abnormal_num' => $currentAbnormalNum, 'abnormal_at' => time()];
+                            }
+                        }
+                        if (!Blog::where('id', $blog->id)->update($data)) {
+                            DB::rollBack();
                         }
                     }
-                    if (!Blog::where('id', $blog->id)->update($data)) {
-                        DB::rollBack();
-                    }
-                }
-            );
-        }
-
-        $results = Promise\unwrap($promises);
-
-        /*if (count($datelines)) {
-            if (!DB::table('dateline')->insert($datelines)) {
-                DB::rollBack();
+                );
             }
-        }*/
+            $results = Promise\unwrap($promises);
+            /*if (count($datelines)) {
+                if (!DB::table('dateline')->insert($datelines)) {
+                    DB::rollBack();
+                }
+            }*/
+        });
 
         DB::commit();
 
